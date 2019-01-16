e55146bb0bdf1b96e19da68763a584b1202d249b
GKampitakis <csd3142@gmail.com>
2019-01-16 14:09:33 +0200

    Added points to array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,17 +16,19 @@ import {
 export class HomePage {
   @ViewChild('map') mapContainer: ElementRef;
   map: any;
+  points:Array<any>=[];
   ionViewDidEnter() {
     this.loadmap();
   }
-  constructor(private statusBar: StatusBar) {}
+  constructor(private statusBar: StatusBar) {
+
+  }
 
   loadmap() {
-
     this.statusBar.overlaysWebView(true);
     this.statusBar.styleDefault();//In order to see the time of the status bar
 
-    var bounds = new leaflet.LatLngBounds(new leaflet.LatLng(0, 0), new leaflet.LatLng(430, 760));
+    var bounds = new leaflet.LatLngBounds(new leaflet.LatLng(0, 0), new leaflet.LatLng(450, 700));//Height,width
     this.map = leaflet.map("map", {
       crs: leaflet.CRS.Simple,
       center: bounds.getCenter(),
@@ -40,33 +42,41 @@ export class HomePage {
     var image = leaflet.imageOverlay('assets/test.jpg', bounds).addTo(this.map);
     this.map.fitBounds(bounds);
 
-    var tail = leaflet.latLng([310, 160]);
-    var nose = leaflet.latLng([310, 400]);
+    var start = leaflet.latLng([310, 160]);
+    var end = leaflet.latLng([310, 400]);
 
-    leaflet.marker(tail).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().on('click', () => {
+    leaflet.marker(start).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().on('click', () => {
       if (this.map.hasLayer(travel)) this.map.removeLayer(travel);
-      else
+      else{
         this.map.addLayer(travel);
+      }
     }).addTo(this.map);
 
-
-    leaflet.marker(nose).addTo(this.map).bindPopup('test').on('click', () => {
-      this.map.removeLayer(travel);
+    leaflet.marker(end).addTo(this.map).bindPopup('test').on('click', () => {
+      if (this.map.hasLayer(travel)) this.map.removeLayer(travel);
+      else{
+        this.map.addLayer(travel);
+      }
     });
     var route = [
-      tail,
+      start,
       leaflet.latLng([274, 135]),
       leaflet.latLng([240, 132]),
       leaflet.latLng([90, 221]),
       leaflet.latLng([10, 245]),
       leaflet.latLng([21, 499]),
       leaflet.latLng([258, 514]),
-      nose,
+      end,
     ]
     var travel = leaflet.polyline(route);
     this.map.on('click', (e) => {
-      console.log("Latitude " + e.latlng.lat + " Longtitude " + e.latlng.lng);
+      this.points.push(leaflet.latLng([274, 135]));
+      console.log(this.points);
     });
 
   };
+
+  clear(){
+    this.points=[];
+  }
 }
